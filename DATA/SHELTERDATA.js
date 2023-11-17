f71af3421c2f61c3bbdafae944e21f067fd4b0c4
Pedro Delgadillo <Pedelgadillo@gmail.com>
2023-11-17 14:26:04 -0600

    added location to the cat table i am creating a cat shelter site, made the table DATA into a json file and added location data and a location drop down menu

---
+++ DATA/SHELTERDATA.js
@@ -0,0 +1,21 @@
+const DATA = [
+  {
+    img: '../resources/img/cat1.jpg',
+    name: 'Whiskers',
+    age: 3,
+    shelterLocation: 'ABC Shelter',
+  },{
+    img: '../resources/img/cat2.jpg',
+    name: 'Ham',
+    age: 2,
+    shelterLocation: 'Shelter Shelter',
+  },
+  {
+    img: '../resources/img/cat3.jpg',
+    name: 'Mittens',
+    age: 15,
+    shelterLocation: 'US Shelter',
+  },
+];
+
+export default DATA;
--- script/cat.js
+++ script/cat.js
@@ -1,36 +1,62 @@
+import DATA from "../DATA/SHELTERDATA.js";
 (() => {
+  const button = document.querySelector("#catSubmit");
   const tableBody = document.querySelector('tbody');
-addCat("../resources/img/cat2.jpg", "Kitty","2.5 years" )
-addCat("../resources/img/cat3.jpg", "Mittens","12 years" )
 
-  function addCat(picture, name, age) {
+
+  //button event 
+  button.addEventListener("click",(event)=>{
+    event.preventDefault(); // Prevent default form submission
+  //caputre form information
+  const catNameInput = document.querySelector("#catName");
+  const catAgeInput = document.querySelector("#catAge");
+  const catImageInput = document.querySelector("#catImage");
+  const catShelterInput = document.querySelector("#catShelter");
+
+  const catName = catNameInput.value;
+  const catAge = catAgeInput.value;
+  const catImage = catImageInput.value;
+  const catShelter = catShelterInput.value;
+
+  addCat(catImage, catName, catAge, catShelter);
+
+  // Clear form inputs after adding cat
+  catNameInput.value = "";
+  catAgeInput.value = "";
+  catImageInput.value = "";
+  } )
+
+  
+  function addCat(picture, name, age, location) {
     //create elements
     const tableRow = document.createElement('tr');
     const tableDataPicture = document.createElement('td');
     const img = document.createElement('img');
     const tableDataName = document.createElement('td');
     const tableDataAge = document.createElement('td');
-
+    const tableDataLocation = document.createElement('td');
     
     //fill elements
     tableDataPicture.className="cat-image-container";
     img.src =picture;
     img.alt = "picture of cat";
     tableDataName.innerText = name;
-    tableDataAge.innerText = age;
-
+    tableDataAge.innerText = `${age} year(s) old`;
+    tableDataLocation.innerText = location;
+    
     // add table data to to table body
     tableDataPicture.appendChild(img);
     tableRow.appendChild(tableDataPicture);
     tableRow.appendChild(tableDataName);
     tableRow.appendChild(tableDataAge);
-
+    tableRow.appendChild(tableDataLocation);
+    
     // add to dom
     tableBody.appendChild(tableRow);
-
   }
 
-
-
+  DATA.forEach(cat => {
+    addCat(cat.img, cat.name,cat.age,cat.shelterLocation )
+  });
 
 })();
